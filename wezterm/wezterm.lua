ddc2a5daa1a0d7796cd837dd478138c5db5b5d3d
ivdi <adhityashan95@gmail.com>
2024-12-09 12:05:16 +0100

    chore: make wezterm translucent

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Visuals
 config.color_scheme = 'Catppuccin Mocha (Gogh)'
+config.window_background_opacity = 0.5
 
 return config
